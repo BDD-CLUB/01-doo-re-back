842816c57eba17a3cfa309874425f5f062654e62
shkisme <shkisme0130@gmail.com>
2024-01-12 22:02:46 +0900

    init: 스프링 어플리케이션 생성

---
+++ src/main/java/doore/DooreApplication.java
@@ -0,0 +1,13 @@
+package doore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DooreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DooreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/doore/DooreApplicationTests.java
@@ -0,0 +1,13 @@
+package doore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DooreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
