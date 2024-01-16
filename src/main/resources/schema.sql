afb876c60fde7b1a10565a0eac5b07badc878631
amaran-th <songsy405@naver.com>
2024-01-16 17:41:49 +0900

    docs: schema.sql 작성
    #9

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,114 @@
+drop table if exists doore.member;
+drop table if exists doore.team;
+drop table if exists doore.study;
+drop table if exists doore.curriculum_item;
+drop table if exists doore.participant_curriculum_item;
+drop table if exists doore.crop;
+drop table if exists doore.team_member;
+drop table if exists doore.participant;
+drop table if exists doore.garden;
+
+
+
+create table member
+(
+    id         bigint auto_increment primary key,
+    name       varchar(255) not null,
+    google_id  varchar(255) not null,
+    email      varchar(255) not null,
+    image_url  varchar(255) not null,
+    is_deleted boolean      not null,
+    created_at datetime(6),
+    updated_at datetime(6)
+);
+
+create table team
+(
+    id          bigint auto_increment primary key,
+    name        varchar(255) not null,
+    description varchar(255) not null,
+    image_url   varchar(255) not null,
+    is_deleted  boolean      not null,
+    created_at  datetime(6),
+    updated_at  datetime(6)
+);
+
+create table study
+(
+    id          bigint auto_increment primary key,
+    name        varchar(255) not null,
+    description varchar(255) not null,
+    start_date  date         not null,
+    end_date    date,
+    status      varchar(255) not null,
+    is_deleted  boolean      not null,
+    team_id     bigint       not null,
+    crop_id     bigint       not null,
+    created_at  datetime(6),
+    updated_at  datetime(6)
+);
+
+create table curriculum_item
+(
+    id         bigint auto_increment primary key,
+    name       varchar(255) not null,
+    order      int          not null,
+    is_deleted boolean      not null,
+    study_id   bigint       not null,
+    created_at datetime(6),
+    updated_at datetime(6)
+);
+
+create table participant_curriculum_item
+(
+    id                 bigint auto_increment primary key,
+    is_checked         boolean not null,
+    is_deleted         boolean not null,
+    curriculum_item_id bigint  not null,
+    participant_id     bigint  not null,
+    created_at         datetime(6),
+    updated_at         datetime(6)
+);
+
+create table crop
+(
+    id         bigint auto_increment primary key,
+    name       varchar(255) not null,
+    image_url  varchar(255) not null,
+    created_at datetime(6),
+    updated_at datetime(6)
+);
+
+create table team_member
+(
+    id         bigint auto_increment primary key,
+    team_id    bigint  not null,
+    member_id  bigint  not null,
+    is_deleted boolean not null,
+    created_at datetime(6),
+    updated_at datetime(6)
+);
+
+create table participant
+(
+    id           bigint auto_increment primary key,
+    study_id     bigint  not null,
+    member_id    bigint  not null,
+    is_completed boolean not null,
+    is_deleted   boolean not null,
+    created_at   datetime(6),
+    updated_at   datetime(6)
+);
+
+create table garden
+(
+    id               bigint auto_increment primary key,
+    contributed_date date         not null,
+    type             varchar(255) not null,
+    is_deleted       boolean      not null,
+    contribution_id  bigint       not null,
+    team_id          bigint       not null,
+    member_id        bigint       not null,
+    created_at       datetime(6),
+    updated_at       datetime(6)
+);
