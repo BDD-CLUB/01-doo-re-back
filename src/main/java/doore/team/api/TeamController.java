3f42900751c9c8874889c7805b61b19f0d8faab4
shkisme <shkisme0130@gmail.com>
2024-01-24 22:22:55 +0900

    refactor: 반환값이 void인 경우에도 통일성을 위해서 HttpResponse를 반환하도록 수정 #13

--- src/main/java/doore/team/api/TeamController.java
+++ src/main/java/doore/team/api/TeamController.java
@@ -6,6 +6,7 @@ import doore.team.application.dto.request.TeamUpdateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
@@ -25,23 +25,23 @@ public class TeamController {
     private final TeamCommandService teamCommandService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createTeam(@Valid @RequestBody final TeamCreateRequest request) {
+    public ResponseEntity<Void> createTeam(@Valid @RequestBody final TeamCreateRequest request) {
         teamCommandService.createTeam(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{teamId}")
-    @ResponseStatus(HttpStatus.OK)
-    public void updateFeed(
+    public ResponseEntity<Void> updateFeed(
             @PathVariable final Long teamId,
             @RequestBody final TeamUpdateRequest request
     ) {
         teamCommandService.updateTeam(teamId, request);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{teamId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTeam(@PathVariable final Long teamId) {
+    public ResponseEntity<Void> deleteTeam(@PathVariable final Long teamId) {
         teamCommandService.deleteTeam(teamId);
+        return ResponseEntity.noContent().build();
     }
 }
