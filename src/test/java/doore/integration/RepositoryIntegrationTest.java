49228e5790cd08a25a6a72f33e57bf4a003f6aab
shkisme <shkisme0130@gmail.com>
2024-01-24 09:14:16 +0900

    test: 팀 삭제 관련 Reposiotry 테스트 작성 #13 - @SQLRestriction, @SQLDelete 어노테이션의 동작을 확인하기 위한 테스트 입니다.

---
+++ src/test/java/doore/integration/RepositoryIntegrationTest.java
@@ -0,0 +1,15 @@
+package doore.integration;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class RepositoryIntegrationTest {
+
+    @PersistenceContext
+    protected EntityManager em;
+
+}
