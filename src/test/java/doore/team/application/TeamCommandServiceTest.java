a01bc97c60d0e06db9f03c4c27ab1dff853aec49
shkisme <shkisme0130@gmail.com>
2024-01-24 09:12:31 +0900

    test: 팀 수정 서비스 실패 테스트 작성 #13

---
+++ src/test/java/doore/team/application/TeamCommandServiceTest.java
@@ -0,0 +1,31 @@
+package doore.team.application;
+
+import static doore.team.exception.TeamExceptionType.NOT_FOUND_TEAM;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import doore.integration.IntegrationTest;
+import doore.team.application.dto.request.TeamUpdateRequest;
+import doore.team.exception.TeamException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TeamCommandServiceTest extends IntegrationTest {
+
+    @Autowired
+    private TeamCommandService teamCommandService;
+
+    @Test
+    @DisplayName("찾을 수 없는 팀은 팀 정보를 수정할 수 없다. [실패]")
+    public void 찾을_수_없는_팀은_팀_정보를_수정할_수_없다_실패() {
+        //given
+        final Long invalidId = 0L;
+        TeamUpdateRequest request = new TeamUpdateRequest("asdf", "asdf");
+
+        //when & then
+        assertThatThrownBy(() -> {
+            teamCommandService.updateTeam(invalidId, request);
+        }).isInstanceOf(TeamException.class)
+                .hasMessage(NOT_FOUND_TEAM.errorMessage());
+    }
+}
