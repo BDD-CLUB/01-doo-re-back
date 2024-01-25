598ef80520fc626adff140e6076208b6d264bb8c
이지민 <min102602@naver.com>
2024-01-26 00:39:52 +0900

    feat: 완료/미완료 관련 메서드 추가 & 빌더 추가

--- src/main/java/doore/study/domain/ParticipantCurriculumItem.java
+++ src/main/java/doore/study/domain/ParticipantCurriculumItem.java
@@ -8,8 +8,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 
 public class ParticipantCurriculumItem extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +28,21 @@ public class ParticipantCurriculumItem extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "curriculum_item_id", nullable = false)
     private CurriculumItem curriculumItem;
+
+    @Builder
+    public ParticipantCurriculumItem(Boolean isChecked, Boolean isDeleted, Long participantId,
+                                     CurriculumItem curriculumItem) {
+        this.isChecked = isChecked;
+        this.isDeleted = isDeleted;
+        this.participantId = participantId;
+        this.curriculumItem = curriculumItem;
+    }
+
+    public void complete() {
+        this.isChecked = true;
+    }
+
+    public void incomplete() {
+        this.isChecked = false;
+    }
 }
