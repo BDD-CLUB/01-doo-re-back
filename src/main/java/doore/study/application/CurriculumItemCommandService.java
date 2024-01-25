0bfff4bcce649d9d0d497d1e7e70a6ae95a7a2c3
이지민 <min102602@naver.com>
2024-01-26 00:36:03 +0900

    feat: `CurriculumsItemCommandService` 구현

---
+++ src/main/java/doore/study/application/CurriculumItemCommandService.java
@@ -0,0 +1,90 @@
+package doore.study.application;
+
+import static doore.study.domain.StudyStatus.IN_PROGRESS;
+import static doore.study.exception.CurriculumItemExceptionType.NOT_FOUND_CURRICULUM_ITEM;
+
+import doore.study.application.dto.request.CurriculumItemRequest;
+import doore.study.domain.CurriculumItem;
+import doore.study.domain.CurriculumItemRepository;
+import doore.study.domain.ParticipantCurriculumItem;
+import doore.study.domain.Study;
+import doore.study.domain.StudyRepository;
+import doore.study.exception.CurriculumItemException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CurriculumItemCommandService {
+
+    private final CurriculumItemRepository curriculumItemRepository;
+    private final StudyRepository studyRepository; // 임시
+    private final AtomicInteger index = new AtomicInteger(1);
+
+    public void createCurriculum(CurriculumItemRequest request) {
+        /// 일단 임시. uri가 잘못된 것 같음 -> Study에서 커리큘럼을 찾는 과정이 필요
+        Study study = Study.builder()
+                .name("123")
+                .description("123")
+                .startDate(LocalDate.now())
+                .status(IN_PROGRESS)
+                .isDeleted(false)
+                .teamId(1L)
+                .cropId(1L)
+                .build();
+        studyRepository.save(study);
+        ///
+
+        CurriculumItem curriculumItem = CurriculumItem.builder()
+                .name(request.name()).itemOrder(index.getAndIncrement()).isDeleted(false).study(study).build();
+        curriculumItemRepository.save(curriculumItem);
+    }
+
+    public void deleteCurriculum(Long curriculumId) {
+        CurriculumItem curriculumItem = curriculumItemRepository.findById(curriculumId)
+                .orElseThrow(() -> new CurriculumItemException(NOT_FOUND_CURRICULUM_ITEM));
+
+        int deletedOrder = curriculumItem.getItemOrder();
+        curriculumItemRepository.delete(curriculumItem);
+
+        List<CurriculumItem> subsequentItems = curriculumItemRepository.findByItemOrderGreaterThan(deletedOrder);
+        for (CurriculumItem item : subsequentItems) {
+            item.updateOrder(item.getItemOrder() - 1);
+        }
+    }
+
+    public void updateCurriculum(Long curriculumId, CurriculumItemRequest request) {
+        CurriculumItem curriculumItem = curriculumItemRepository.findById(curriculumId)
+                .orElseThrow(() -> new CurriculumItemException(NOT_FOUND_CURRICULUM_ITEM));
+        curriculumItem.update(request.name());
+    }
+
+    public void completeCurriculum(Long curriculumId) {
+        CurriculumItem curriculumItem = curriculumItemRepository.findById(curriculumId)
+                .orElseThrow(() -> new CurriculumItemException(NOT_FOUND_CURRICULUM_ITEM));
+        ParticipantCurriculumItem participantCurriculumItem = ParticipantCurriculumItem.builder()
+                .isChecked(false)
+                .isDeleted(false)
+                .participantId(1L) // 임시. url을 수정하거나, 다른 정보로 받아오거나
+                .curriculumItem(curriculumItem)
+                .build();
+        participantCurriculumItem.complete();
+    }
+
+    public void incompleteCurriculum(Long curriculumId) {
+        CurriculumItem curriculumItem = curriculumItemRepository.findById(curriculumId)
+                .orElseThrow(() -> new CurriculumItemException(NOT_FOUND_CURRICULUM_ITEM));
+        ParticipantCurriculumItem participantCurriculumItem = ParticipantCurriculumItem.builder()
+                .isChecked(true)
+                .isDeleted(false)
+                .participantId(1L) // 임시. url을 수정하거나, 다른 정보로 받아오거나
+                .curriculumItem(curriculumItem)
+                .build();
+        participantCurriculumItem.incomplete();
+    }
+}
