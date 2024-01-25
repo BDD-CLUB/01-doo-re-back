f1f0685248adefa4d685a3357ae05a85ac9a8446
amaran-th <songsy405@naver.com>
2024-01-25 15:37:41 +0900

    feat: 서비스 클래스에 @Transactional 추가
    #10

--- src/main/java/doore/member/application/MemberCommandService.java
+++ src/main/java/doore/member/application/MemberCommandService.java
@@ -5,14 +5,16 @@ import doore.member.domain.Member;
 import doore.member.domain.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @AllArgsConstructor
 public class MemberCommandService {
 
     private final MemberRepository memberRepository;
 
-    // TODO: 1/23/24 추후 확장성 관련해서 논의
+    // TODO: 1/23/24 추후 소셜 로그인 플랫폼이 늘어나는 경우의 확장성 관련해서 논의
     public Member findOrCreateMemberBy(final GoogleAccountProfileResponse profile) {
         return memberRepository.findByGoogleId(profile.getId())
                 .orElse(memberRepository.save(
