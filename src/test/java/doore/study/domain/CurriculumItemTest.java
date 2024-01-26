ee7f2eb03e6de6f3c680a47c6f205f7bba5dad73
이지민 <min102602@naver.com>
2024-01-26 13:15:50 +0900

    test: 완료 및 미완료 테스트 추가

--- src/test/java/doore/study/domain/CurriculumItemTest.java
+++ src/test/java/doore/study/domain/CurriculumItemTest.java
@@ -18,4 +18,28 @@ public class CurriculumItemTest {
 
         assertThat(curriculumItem.getName()).isEqualTo(changeName);
     }
+
+    @Test
+    @DisplayName("커리큘럼이 완료 상태로 변경된다. [성공]")
+    public void 커리큘럼이_완료_상태로_변경된다_성공() {
+        CurriculumItem curriculumItem = CurriculumItemFixture.curriculumItem();
+        ParticipantCurriculumItem participantCurriculumItem = ParticipantCurriculumItem.builder()
+                .isChecked(false).isDeleted(false).participantId(1L).curriculumItem(curriculumItem).build();
+
+        participantCurriculumItem.complete();
+
+        assertThat(participantCurriculumItem.getIsChecked()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("커리큘럼이 미완료 상태로 변경된다. [성공]")
+    public void 커리큘럼이_미완료_상태로_변경된다_성공() {
+        CurriculumItem curriculumItem = CurriculumItemFixture.curriculumItem();
+        ParticipantCurriculumItem participantCurriculumItem = ParticipantCurriculumItem.builder()
+                .isChecked(true).isDeleted(false).participantId(1L).curriculumItem(curriculumItem).build();
+
+        participantCurriculumItem.incomplete();
+
+        assertThat(participantCurriculumItem.getIsChecked()).isEqualTo(false);
+    }
 }
