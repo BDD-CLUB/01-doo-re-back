8452d68dc51231dad0b7b4d6edff918ca9f6ba32
amaran-th <songsy405@naver.com>
2024-01-26 14:21:22 +0900

    refactor: 불필요한 @Repository 어노테이션 제거
    #10

--- src/main/java/doore/member/domain/repository/MemberRepository.java
+++ src/main/java/doore/member/domain/repository/MemberRepository.java
@@ -3,9 +3,7 @@ package doore.member.domain.repository;
 import doore.member.domain.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByGoogleId(final String googleId);
