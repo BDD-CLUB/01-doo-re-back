ca7ef1240e2fa17f404187a10bfc4028af1103fd
lcqff <yeonnu82g@gmail.com>
2024-01-28 15:36:04 +0900

    refactor: Controller 메서드는 ResponseEntity를 반환하도록 한다.
    #14

--- src/main/java/doore/study/api/StudyController.java
+++ src/main/java/doore/study/api/StudyController.java
@@ -8,6 +8,7 @@ import doore.study.application.dto.request.StudyCreateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,32 +28,35 @@ public class StudyController {
     private final StudyQueryService studyQueryService;
 
     @PostMapping("/teams/{teamId}/studies")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createStudy(@Valid @RequestBody StudyCreateRequest studyRequest, @PathVariable Long teamId) {
+    public ResponseEntity<Void> createStudy(@Valid @RequestBody StudyCreateRequest studyRequest, @PathVariable Long teamId) {
         studyCommandService.createStudy(studyRequest, teamId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/studies/{studyId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteStudy(@PathVariable Long studyId) {
+    public ResponseEntity<Void> deleteStudy(@PathVariable Long studyId) {
         studyCommandService.deleteStudy(studyId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @GetMapping("/studies/{studyId}")
     @ResponseStatus(HttpStatus.OK)
-    public StudyDetailResponse getStudy(@PathVariable Long studyId) {
-        return studyQueryService.findStudyById(studyId);
+    public ResponseEntity<StudyDetailResponse> getStudy(@PathVariable Long studyId) {
+        StudyDetailResponse studyDetailResponse = studyQueryService.findStudyById(studyId);
+        return ResponseEntity.ok(studyDetailResponse);
     }
 
     @PutMapping("/studies/{studyId}")
     @ResponseStatus(HttpStatus.OK)
-    public void updateStudy(@Valid @RequestBody StudyUpdateRequest studyUpdateRequest, @PathVariable Long studyId) {
+    public ResponseEntity<Void> updateStudy(@Valid @RequestBody StudyUpdateRequest studyUpdateRequest, @PathVariable Long studyId) {
         studyCommandService.updateStudy(studyUpdateRequest, studyId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PatchMapping("/studies/{studyId}/termination")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void terminateStudy(@PathVariable Long studyId) {
+    public ResponseEntity<Void> terminateStudy(@PathVariable Long studyId) {
         studyCommandService.terminateStudy(studyId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
