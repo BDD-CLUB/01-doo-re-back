08872f9736fdd3eadae8c2aef2b8f318b55ffcc8
이지민 <min102602@naver.com>
2024-01-30 23:33:25 +0900

    feat: `CurriculumItemRepository` 생성

---
+++ src/main/java/doore/study/domain/ParticipantCurriculumItemRepository.java
@@ -0,0 +1,6 @@
+package doore.study.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantCurriculumItemRepository extends JpaRepository<ParticipantCurriculumItem, Long> {
+}
