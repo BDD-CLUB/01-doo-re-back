569ffc8a51c523c32ca6e9c729be4c6da86eddf6
JJimini <min102602@pusan.ac.kr>
2024-02-08 13:53:53 +0900

    fix: controller test 삭제 - api docs test와 중복된 내용의 테스트로, 삭제하였습니다.

--- src/test/java/doore/study/api/CurriculumItemControllerTest.java
+++ src/test/java/doore/study/api/CurriculumItemControllerTest.java
@@ -4,13 +4,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import doore.helper.IntegrationTest;
 import doore.member.domain.repository.ParticipantRepository;
-import doore.study.CurriculumItemFixture;
 import doore.study.StudyFixture;
-import doore.study.application.dto.request.CurriculumItemRequest;
+import doore.study.application.dto.request.CurriculumItemManageRequest;
 import doore.study.domain.CurriculumItem;
 import doore.study.domain.Study;
 import doore.study.domain.repository.CurriculumItemRepository;
 import doore.study.domain.repository.StudyRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,46 +27,48 @@ public class CurriculumItemControllerTest extends IntegrationTest {
     protected ParticipantRepository participantRepository;
 
     private Study study;
+    private CurriculumItem curriculumItem1;
+    private CurriculumItem curriculumItem2;
+    private CurriculumItem curriculumItem3;
+    private CurriculumItemManageRequest request;
 
     @BeforeEach
     void setUp() {
         study = StudyFixture.algorithmStudy();
         studyRepository.save(study);
+
+        curriculumItem1 = CurriculumItem.builder().id(1L).itemOrder(1).name("Spring Study").study(study).build();
+        curriculumItemRepository.save(curriculumItem1);
+        curriculumItem2 = CurriculumItem.builder().id(2L).itemOrder(2).name("CS Study").study(study).build();
+        curriculumItemRepository.save(curriculumItem2);
+        curriculumItem3 = CurriculumItem.builder().id(3L).itemOrder(3).name("Infra Study").study(study).build();
+        curriculumItemRepository.save(curriculumItem3);
+
+        request = CurriculumItemManageRequest.builder()
+                .curriculumItems(getCurriculumItems())
+                .deletedCurriculumItems(getDeletedCurriculumItems())
+                .build();
+    }
+
+    private List<CurriculumItem> getCurriculumItems() {
+        List<CurriculumItem> curriculumItems = new ArrayList<>();
+        curriculumItems.add(CurriculumItem.builder().id(1L).itemOrder(1).name("Change Spring Study").build());
+        curriculumItems.add(CurriculumItem.builder().id(2L).itemOrder(4).name("CS Study").build());
+        curriculumItems.add(CurriculumItem.builder().id(3L).itemOrder(2).name("Infra Study").build());
+        curriculumItems.add(CurriculumItem.builder().id(4L).itemOrder(3).name("Algorithm Study").build());
+        return curriculumItems;
+    }
+
+    private List<CurriculumItem> getDeletedCurriculumItems() {
+        List<CurriculumItem> deletedCurriculumItems = new ArrayList<>();
+        deletedCurriculumItems.add(CurriculumItem.builder().id(3L).itemOrder(2).name("Infra Study").build());
+        return deletedCurriculumItems;
     }
 
     @Test
-    @DisplayName("[성공] 커리큘림이 정상 등록된다.")
-    public void createCurriculum_커리큘림이_정상_등록된다_성공() throws Exception {
-        CurriculumItemRequest request = new CurriculumItemRequest("Spring Study");
+    @DisplayName("[성공] 커리큘럼 관리가 정상적으로 이루어진다.")
+    public void manageCurriculum_커리큘럼_관리가_정상적으로_이루어진다 () throws Exception {
         String url = "/studies/" + study.getId() + "/curriculums";
         callPostApi(url, request).andExpect(status().isCreated());
     }
-
-    @Test
-    @DisplayName("[실패] 필수 정보를 입력하지 않았다면 커리큘럼 등록은 실패한다.")
-    public void createCurriculum_필수_정보를_입력하지_않았다면_커리큘럼_등록은_실패한다_실패() throws Exception {
-        CurriculumItemRequest request = new CurriculumItemRequest(null);
-        String url = "/studies/" + study.getId() + "/curriculums";
-        callPostApi(url, request).andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("[성공] 커리큘럼이 정상 삭제된다.")
-    public void deleteCurriculum_커리큘럼이_정상_삭제된다_성공() throws Exception {
-        CurriculumItem curriculumItem = CurriculumItemFixture.curriculumItem();
-        curriculumItemRepository.save(curriculumItem);
-        String url = "/curriculums/" + curriculumItem.getId();
-        callDeleteApi(url).andExpect(status().isNoContent());
-    }
-
-    @Test
-    @DisplayName("[성공] 커리큘럼이 정상 수정된다.")
-    public void updateCurriculum_커리큘럼이_정상_수정된다_성공() throws Exception {
-        CurriculumItem curriculumItem = CurriculumItemFixture.curriculumItem();
-        curriculumItemRepository.save(curriculumItem);
-        CurriculumItemRequest request = new CurriculumItemRequest("Change Spring Study");
-        String url = "/curriculums/" + curriculumItem.getId();
-        callPatchApi(url, request).andExpect(status().isNoContent());
-    }
-
 }
