74e1fad147d3af971b2089e7d15dda555d6179bc
lcqff <yeonnu82g@gmail.com>
2024-02-16 15:49:26 +0900

    refactor: 순서 기반 쿼리를 이름 기반 쿼리로 변경
    #78

--- src/main/java/doore/attendance/domain/repository/AttendanceRepository.java
+++ src/main/java/doore/attendance/domain/repository/AttendanceRepository.java
@@ -4,9 +4,9 @@ import doore.attendance.domain.Attendance;
 import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-    @Query(value = "select count(*)>0 from Attendance a where a.memberId= ?1 and DATE(a.createdAt)=?2")
-    Boolean existsByMemberIdAndDate(Long memberId, LocalDate date);
-
+    @Query(value = "select count(*)>0 from Attendance a where a.memberId= :memberId and DATE(a.createdAt)=:date")
+    Boolean existsByMemberIdAndDate(@Param("memberId")Long memberId, @Param("date")LocalDate date);
 }
--- src/test/java/doore/attendance/domain/AttendanceRepositoryTest.java
+++ src/test/java/doore/attendance/domain/AttendanceRepositoryTest.java
@@ -1,4 +1,4 @@
-package doore.domain;
+package doore.attendance.domain;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
