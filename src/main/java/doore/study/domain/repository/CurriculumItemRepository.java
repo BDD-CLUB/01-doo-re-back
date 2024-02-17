dd8b885cd0c77bff28760cbc5ceb52316d074e47
JIMIN LEE <107912763+JJimini@users.noreply.github.com>
2024-02-17 13:28:33 +0900

    Feature/#75 커리큘럼 관리 기능 구현 (#81)
    * feat: 커리큘럼 관리 기능 구현
    
    * test: 커리큘럼 관리 기능 test 구현
    
    * feat: itemOrder 유효성 코드 추가
    - itemOrder는 중복이 불가합니다.
    - itemOrder는 Integer 범위를 넘어갈 수 없습니다.
    
    * docs: 커리큘럼 관리 api 문서 작성
    
    * fix: 참여자 예외처리 추가
    
    * fix: 참여자 예외처리 추가2
    
    * fix: controller test 삭제
    - api docs test와 중복된 내용의 테스트로, 삭제하였습니다.
    
    * fix: controllerTest 삭제 (최종)
    
    * refactor : itemOrder 유효성 검사 메서드 분리
    
    * refactor : name 및 itemOrder 업데이트 관련 메서드 분리
    
    * refactor : 정렬 방법 변경
    
    * refactor : itemOrder 재부여 코드를 IntStream을 활용한 방법으로 변경
    
    * refactor: 리뷰 수정사항 수정
    
    * fix: 커리큘럼 체크 오류 수정
    - 커리큘럼 url에서 participantId를 받아옵니다.
    - 참여자가 직접 체크하는 방식이므로 참여자Id를 통해 정보를 찾아와 변경합니다.
    
    * test: url 수정에 따른 테스트 수정
    
    * test: 커리큘럼 상태 변경 테스트 코드 추가
    
    * docs: 커리큘럼 관리 관련 문서 추가
    
    * fix: 중복된 테스트 요소 삭제
    
    * fix: 커리큘럼 상태 변경 시 참여자아이디와 커리큘럼 아이디를 모두 활용하는 방식으로 변경

--- src/main/java/doore/study/domain/repository/CurriculumItemRepository.java
+++ src/main/java/doore/study/domain/repository/CurriculumItemRepository.java
@@ -5,6 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurriculumItemRepository extends JpaRepository<CurriculumItem, Long> {
-
-    List<CurriculumItem> findByItemOrderGreaterThan(int itemOrder);
+    List<CurriculumItem> findAllByOrderByItemOrderAsc();
 }
