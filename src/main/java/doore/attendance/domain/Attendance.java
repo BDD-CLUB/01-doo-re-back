956d08a1d435cf51557359de8188fff63eab8cb4
lcqff <yeonnu82g@gmail.com>
2024-02-22 04:34:25 +0900

    refactor: 필드에 nullable=false 적용
    #78

--- src/main/java/doore/attendance/domain/Attendance.java
+++ src/main/java/doore/attendance/domain/Attendance.java
@@ -1,6 +1,7 @@
 package doore.attendance.domain;
 
 import doore.base.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Attendance extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Long memberId;
 
     @Builder
