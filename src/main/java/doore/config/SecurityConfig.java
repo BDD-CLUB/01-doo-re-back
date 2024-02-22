f151c300ee5167a8b6a03178dab390c317cc2a35
JJimini <min102602@pusan.ac.kr>
2024-02-23 00:09:48 +0900

    feat: 권한 관련 시큐리티 설정

---
+++ src/main/java/doore/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package doore.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/teams/**").hasAnyRole("팀장", "팀원")
+                                .requestMatchers("/studies/**").hasAnyRole("스터디장", "스터디원")
+                                .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
