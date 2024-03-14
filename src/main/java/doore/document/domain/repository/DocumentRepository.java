4f62879a7d17b06a8cadf7f6c0a953ce194bd53e
lcqff <yeonnu82g@gmail.com>
2024-03-14 17:47:52 +0900

    feat: 팀 랭킹 조회 구현
    #99

--- src/main/java/doore/document/domain/repository/DocumentRepository.java
+++ src/main/java/doore/document/domain/repository/DocumentRepository.java
@@ -5,7 +5,22 @@ import doore.document.domain.DocumentGroupType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DocumentRepository extends JpaRepository<Document,Long> {
+    @Query(value = "select COUNT(*) from Document document "
+            + "where document.groupType = 'TEAM' "
+            + "and document.groupId = :teamId "
+            + "and document.isDeleted = false "
+            + "and Date(document.createdAt) = CURRENT DATE ")
+    int getTodayUploadedDocumentFromTeam(Long teamId);
+
+    @Query(value = "select COUNT(*) from Document document "
+            + "where document.groupType = 'TEAM' "
+            + "and document.groupId = :teamId "
+            + "and document.isDeleted = false "
+            + "and Date(document.createdAt) >= ADDDATE(CURDATE(), -DAYOFWEEK(CURDATE())+2) ")
+    int getWeekUploadedDocumentFromTeam(Long teamId);
+
     Page<Document> findAllByGroupTypeAndGroupId(DocumentGroupType groupType, Long groupId, Pageable pageable);
 }
--- src/main/java/doore/study/domain/repository/ParticipantCurriculumItemRepository.java
+++ src/main/java/doore/study/domain/repository/ParticipantCurriculumItemRepository.java
@@ -1,9 +1,33 @@
 package doore.study.domain.repository;
 
 import doore.study.domain.ParticipantCurriculumItem;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ParticipantCurriculumItemRepository extends JpaRepository<ParticipantCurriculumItem, Long> {
     Optional<ParticipantCurriculumItem> findByCurriculumItemIdAndParticipantId(Long curriculumId, Long participantId);
+
+    @Query(value = "select COUNT(*) from ParticipantCurriculumItem pci "
+            + "join CurriculumItem ci on pci.curriculumItem.id = ci.id "
+            + "join Study s on ci.study.id = s.id "
+            + "where s.teamId = :teamId "
+            + "and pci.isChecked = true "
+            + "and Date(pci.updatedAt) = CURRENT DATE "
+            + "and pci.isDeleted = false "
+            + "and ci.isDeleted = false "
+            + "and s.isDeleted = false")
+    int getTodayCheckedCurriculumItemFromTeam(Long teamId);
+
+    @Query(value = "select COUNT(*) from ParticipantCurriculumItem pci "
+            + "join CurriculumItem ci on pci.curriculumItem.id = ci.id "
+            + "join Study s on ci.study.id = s.id "
+            + "where s.teamId = :teamId "
+            + "and pci.isChecked = true "
+            + "and Date(pci.updatedAt) >= ADDDATE(CURDATE(), -DAYOFWEEK(CURDATE())+2) "
+            + "and pci.isDeleted = false "
+            + "and ci.isDeleted = false "
+            + "and s.isDeleted = false")
+    int getWeekCheckedCurriculumItemFromTeam(Long teamId);
 }
