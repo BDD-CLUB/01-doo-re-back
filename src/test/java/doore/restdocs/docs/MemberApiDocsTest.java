939fb3318695596c47b4510d0aeab6ad5693328a
JJimini <min102602@pusan.ac.kr>
2024-03-21 00:45:15 +0900

    docs: 권한 위임 관련 문서 작성

---
+++ src/test/java/doore/restdocs/docs/MemberApiDocsTest.java
@@ -0,0 +1,38 @@
+package doore.restdocs.docs;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import doore.member.api.MemberController;
+import doore.restdocs.RestDocsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(MemberController.class)
+public class MemberApiDocsTest extends RestDocsTest {
+
+    @Test
+    @DisplayName("[성공] 유효한 요청이면 팀장 권한이 정상적으로 위임된다.")
+    void transferTeamMaster_유효한_요청이면_팀장_권한이_정상적으로_위임된다() throws Exception {
+        doNothing().when(memberCommandService).transferTeamMaster(any(), any());
+
+        mockMvc.perform(patch("/teams/{teamsId}/mandate", 1))
+                .andExpect(status().isNoContent())
+                .andDo(document("transfer-team-master"));
+    }
+
+    @Test
+    @DisplayName("[성공] 유효한 요청이면 스터디장 권한이 정상적으로 위임된다.")
+    void transferStudyMaster_유효한_요청이면_스터디장_권한이_정상적으로_위임된다() throws Exception {
+        doNothing().when(memberCommandService).transferStudyMaster(any(), any());
+
+        mockMvc.perform(patch("/study/{studyId}/mandate", 1))
+                .andExpect(status().isNoContent())
+                .andDo(document("transfer-study-master"));
+    }
+
+}
