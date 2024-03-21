3a1be7f9a531b0aba138859dcce9d9abcdc415d0
amaran-th <songsy405@naver.com>
2024-03-21 13:50:16 +0900

    feat: 스터디 관련 Response에 정적 팩토리 메서드 구현
    #94

--- src/main/java/doore/study/application/dto/response/StudyResponse.java
+++ src/main/java/doore/study/application/dto/response/StudyResponse.java
@@ -1,10 +1,12 @@
 package doore.study.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import doore.crop.domain.Crop;
 import doore.crop.response.CropReferenceResponse;
+import doore.study.domain.Study;
 import doore.study.domain.StudyStatus;
 import doore.team.application.dto.response.TeamReferenceResponse;
-import jakarta.persistence.Embeddable;
+import doore.team.domain.Team;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,4 +30,10 @@ public class StudyResponse {
     Boolean isDeleted;
     TeamReferenceResponse teamReference;
     CropReferenceResponse cropReference;
+
+    public static StudyResponse of(final Study study, final Team team, final Crop crop) {
+        return new StudyResponse(study.getId(), study.getName(), study.getDescription(), study.getStartDate(),
+                study.getEndDate(), study.getStatus(), study.getIsDeleted(),
+                TeamReferenceResponse.from(team), CropReferenceResponse.from(crop));
+    }
 }
