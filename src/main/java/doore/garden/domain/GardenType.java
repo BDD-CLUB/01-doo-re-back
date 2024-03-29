0cf9906800304ff9ecab5adddfb295dbc1ff7770
lcqff <yeonnu82g@gmail.com>
2024-03-29 17:02:43 +0900

    feat: GardenType Supplier 수정, enum 이름 변경
    - Enum은 명사형으로 작명한다.
    - 클래스에 따라 다른 GardenType을 가져오는 로직을 getSupplierOf에서 분리한다.
    
    #113

--- src/main/java/doore/garden/domain/GardenType.java
+++ src/main/java/doore/garden/domain/GardenType.java
@@ -9,19 +9,23 @@ import lombok.Getter;
 @Getter
 public enum GardenType {
     DOCUMENT_UPLOAD("Document", ContributionGarden::new),
-    COMPLETE_STUDY_CURRICULUM("ParticipantCurriculumItem", ContributionGarden::new);
+    STUDY_CURRICULUM_COMPLETION("ParticipantCurriculumItem", ContributionGarden::new);
 
-    private final String type;
+    private final String contributionClass;
     private final Supplier<ContributionGarden> supplier;
     private static final Map<String, GardenType> typeMap = Arrays.stream(GardenType.values())
-            .collect(Collectors.toMap(GardenType::getType, gardenType -> gardenType));
+            .collect(Collectors.toMap(GardenType::getContributionClass, gardenType -> gardenType));
 
-    GardenType(String type, Supplier<ContributionGarden> supplier) {
-        this.type = type;
+    GardenType(String contributionClass, Supplier<ContributionGarden> supplier) {
+        this.contributionClass = contributionClass;
         this.supplier = supplier;
     }
 
-    public static ContributionGarden getSupplierOf(String type) {
-        return typeMap.get(type).getSupplier().get();
+    public static GardenType getGardenTypeOf(String contributionClass) {
+        return typeMap.get(contributionClass);
+    }
+
+    public static ContributionGarden getSupplierOf(String contributionClass) {
+        return getGardenTypeOf(contributionClass).getSupplier().get();
     }
 }
