29d05ab9f3999e9650c090fb4d78d0b244f31f87
JJimini <min102602@pusan.ac.kr>
2024-05-19 22:31:07 +0900

    feat: 참여자 api 권한 부여

--- src/main/java/doore/study/api/ParticipantController.java
+++ src/main/java/doore/study/api/ParticipantController.java
@@ -1,12 +1,10 @@
 package doore.study.api;
 
-import static doore.member.exception.MemberExceptionType.UNAUTHORIZED;
-
+import doore.member.domain.Member;
 import doore.member.domain.Participant;
-import doore.member.exception.MemberException;
+import doore.resolver.LoginMember;
 import doore.study.application.ParticipantCommandService;
 import doore.study.application.ParticipantQueryService;
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,25 +22,22 @@ public class ParticipantController {
     private final ParticipantQueryService participantQueryService;
 
     @PostMapping("/studies/{studyId}/members/{memberId}")
-    public ResponseEntity<Void> saveParticipant(@PathVariable Long studyId, @PathVariable Long memberId) {
-        participantCommandService.saveParticipant(studyId, memberId);
+    public ResponseEntity<Void> saveParticipant(@PathVariable Long studyId, @PathVariable Long memberId,
+                                                @LoginMember Member member) {
+        participantCommandService.saveParticipant(studyId, memberId, member.getId());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/studies/{studyId}/members/{memberId}")
-    public ResponseEntity<Void> deleteParticipant(@PathVariable Long studyId, @PathVariable Long memberId) {
-        participantCommandService.deleteParticipant(studyId, memberId);
+    public ResponseEntity<Void> deleteParticipant(@PathVariable Long studyId, @PathVariable Long memberId,
+                                                  @LoginMember Member member) {
+        participantCommandService.deleteParticipant(studyId, memberId, member.getId());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/studies/{studyId}/members")
-    public ResponseEntity<Void> withdrawParticipant(@PathVariable Long studyId, HttpServletRequest request) {
-        //Todo: 이후 권한 로직으로 수정
-        String memberId = request.getHeader("Authorization");
-        if (memberId == null) {
-            throw new MemberException(UNAUTHORIZED);
-        }
-        participantCommandService.withdrawParticipant(studyId, Long.parseLong(memberId));
+    public ResponseEntity<Void> withdrawParticipant(@PathVariable Long studyId, @LoginMember Member member) {
+        participantCommandService.withdrawParticipant(studyId, member.getId(), member.getId());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
--- src/main/java/doore/study/application/ParticipantCommandService.java
+++ src/main/java/doore/study/application/ParticipantCommandService.java
@@ -1,18 +1,20 @@
 package doore.study.application;
 
+import static doore.member.domain.StudyRoleType.ROLE_스터디장;
 import static doore.member.exception.MemberExceptionType.NOT_FOUND_MEMBER;
+import static doore.member.exception.MemberExceptionType.NOT_FOUND_MEMBER_ROLE_IN_STUDY;
 import static doore.member.exception.MemberExceptionType.UNAUTHORIZED;
 import static doore.study.exception.StudyExceptionType.NOT_FOUND_STUDY;
 
 import doore.member.domain.Member;
 import doore.member.domain.Participant;
+import doore.member.domain.StudyRole;
 import doore.member.domain.repository.MemberRepository;
 import doore.member.domain.repository.ParticipantRepository;
+import doore.member.domain.repository.StudyRoleRepository;
 import doore.member.exception.MemberException;
-import doore.study.domain.Study;
 import doore.study.domain.repository.StudyRepository;
 import doore.study.exception.StudyException;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,8 +26,10 @@ public class ParticipantCommandService {
     private final StudyRepository studyRepository;
     private final ParticipantRepository participantRepository;
     private final MemberRepository memberRepository;
+    private final StudyRoleRepository studyRoleRepository;
 
-    public void saveParticipant(Long studyId, Long memberId) {
+    public void saveParticipant(Long studyId, Long memberId, Long studyLeaderId) {
+        validateExistStudyLeader(studyLeaderId);
         validateExistStudy(studyId);
         Member member = validateExistMember(memberId);
         Participant participant = Participant.builder()
@@ -35,13 +39,15 @@ public class ParticipantCommandService {
         participantRepository.save(participant);
     }
 
-    public void deleteParticipant(Long studyId, Long memberId) {
+    public void deleteParticipant(Long studyId, Long memberId, Long studyLeaderId) {
+        validateExistStudyLeader(studyLeaderId);
         validateExistStudy(studyId);
         Member member = validateExistMember(memberId);
         participantRepository.deleteByStudyIdAndMember(studyId, member);
     }
 
-    public void withdrawParticipant(Long studyId, Long memberId) {
+    public void withdrawParticipant(Long studyId, Long memberId, Long studyLeaderId) {
+        validateExistStudyLeader(studyLeaderId);
         validateExistStudy(studyId);
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
@@ -54,5 +60,13 @@ public class ParticipantCommandService {
 
     private Member validateExistMember(Long memberId) {
         return memberRepository.findById(memberId).orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
+    }
+
+    private void validateExistStudyLeader(Long memberId) {
+        StudyRole studyRole = studyRoleRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER_ROLE_IN_STUDY));
+        if (!studyRole.getStudyRoleType().equals(ROLE_스터디장)){
+            throw new MemberException(UNAUTHORIZED);
+        }
     }
 }
