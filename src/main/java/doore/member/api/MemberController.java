c80cc4cd6834cf94ba390112c97ba3ebd72d26af
JJimini <min102602@pusan.ac.kr>
2024-05-22 22:58:35 +0900

    chore : member 권한 주석 추가

--- src/main/java/doore/member/api/MemberController.java
+++ src/main/java/doore/member/api/MemberController.java
@@ -18,21 +18,21 @@ public class MemberController {
 
     private final MemberCommandService memberCommandService;
 
-    @PatchMapping("/teams/{teamId}/mandate/{newTeamLeaderId}")
+    @PatchMapping("/teams/{teamId}/mandate/{newTeamLeaderId}") // 팀장
     public ResponseEntity<Void> transferTeamLeader(@PathVariable Long teamId, @PathVariable Long newTeamLeaderId,
                                                    @LoginMember Member member) {
         memberCommandService.transferTeamLeader(teamId, newTeamLeaderId, member.getId());
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/study/{studyId}/mandate/{newStudyLeaderId}")
+    @PatchMapping("/study/{studyId}/mandate/{newStudyLeaderId}") // 스터디장
     public ResponseEntity<Void> transferStudyLeader(@PathVariable Long studyId, @PathVariable Long newStudyLeaderId,
                                                     @LoginMember Member member) {
         memberCommandService.transferStudyLeader(studyId, newStudyLeaderId, member.getId());
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/members")
+    @DeleteMapping("/members") // 회원
     public ResponseEntity<Void> deleteMember(@LoginMember Member member) {
         memberCommandService.deleteMember(member.getId());
         return ResponseEntity.noContent().build();
