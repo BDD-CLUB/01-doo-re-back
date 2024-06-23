cd8a223f582612820f371b652812d08b3abf501a
JJimini <min102602@pusan.ac.kr>
2024-06-23 23:18:50 +0900

    refactor: crop test final 수정

--- src/test/java/doore/crop/CropFixture.java
+++ src/test/java/doore/crop/CropFixture.java
@@ -2,13 +2,6 @@ package doore.crop;
 
 import doore.crop.domain.Crop;
 import doore.crop.domain.repository.CropRepository;
-import doore.study.StudyFixture;
-import doore.study.domain.CurriculumItem;
-import doore.study.domain.Study;
-import doore.study.domain.StudyStatus;
-import doore.team.TeamFixture;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +10,7 @@ public class CropFixture {
     private static CropRepository cropRepository;
 
     @Autowired
-    public CropFixture(CropRepository cropRepository) {
+    public CropFixture(final CropRepository cropRepository) {
         CropFixture.cropRepository = cropRepository;
     }
     public static Crop createCrop() {
