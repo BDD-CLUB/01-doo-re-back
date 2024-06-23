bb92d6b803f9a9babb3484099588d195a249228a
JJimini <min102602@pusan.ac.kr>
2024-06-23 23:22:07 +0900

    refactor: member test final 추가 수정

--- src/test/java/doore/member/domain/TeamRoleRepositoryTest.java
+++ src/test/java/doore/member/domain/TeamRoleRepositoryTest.java
@@ -27,11 +27,11 @@ public class TeamRoleRepositoryTest extends RepositorySliceTest {
     @Test
     @DisplayName("[성공] 팀 정보와 멤버 정보가 유효하다면 팀 내 역할을 조회할 수 있다")
     void findTeamRoleByTeamIdAndMemberId_팀_정보와_멤버_정보가_유효하다면_팀_내_역할을_조회할_수_있다() {
-        Team team = TeamFixture.team();
+        final Team team = TeamFixture.team();
         teamRepository.save(team);
-        Member member = MemberFixture.아마란스();
+        final Member member = MemberFixture.아마란스();
         memberRepository.save(member);
-        TeamRole teamRole = TeamRole.builder()
+        final TeamRole teamRole = TeamRole.builder()
                 .teamId(team.getId())
                 .teamRoleType(ROLE_팀원)
                 .memberId(member.getId())
@@ -40,7 +40,8 @@ public class TeamRoleRepositoryTest extends RepositorySliceTest {
         em.flush();
         em.clear();
 
-        Optional<TeamRole> result = teamRoleRepository.findTeamRoleByTeamIdAndMemberId(team.getId(), member.getId());
+        final Optional<TeamRole> result = teamRoleRepository.findTeamRoleByTeamIdAndMemberId(team.getId(),
+                member.getId());
 
         assertThat(result).isNotEmpty();
     }
