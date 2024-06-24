9e0d8691c82692ab15e323a36ff18cb1dde49c56
lcqff <yeonnu82g@gmail.com>
2024-06-24 18:23:41 +0900

    feat: 팀 랭킹 조회시 텃밭 데이터 추가
    #149

--- src/main/java/doore/team/application/TeamQueryService.java
+++ src/main/java/doore/team/application/TeamQueryService.java
@@ -23,10 +23,8 @@ import doore.team.domain.Team;
 import doore.team.domain.TeamRepository;
 import doore.team.exception.TeamException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -97,22 +95,22 @@ public class TeamQueryService {
     }
 
     public List<TeamRankResponse> getTeamRanks() {
-        Map<Integer, TeamReferenceResponse> teamRanks = calculateTeamRanks();
-        return teamRanks.entrySet().stream()
-                .map(entry -> new TeamRankResponse(entry.getKey(), entry.getValue()))
+        List<Team> teams = teamRepository.findAll();
+        List<TeamRankResponse> teamRanks = new ArrayList<>();
+        for (Team team : teams) {
+            final int point = calculatePoint(team);
+            final List<DayGardenResponse> yearGardenResponses = gardenQueryService.getAllGarden(team.getId());
+            teamRanks.add(new TeamRankResponse(point, TeamReferenceResponse.from(team), yearGardenResponses));
+        }
+        return teamRanks.stream()
+                .sorted(Comparator.comparingInt(TeamRankResponse::point).reversed())
                 .toList();
     }
 
-    private Map<Integer, TeamReferenceResponse> calculateTeamRanks() {
-        List<Team> teams = teamRepository.findAll();
-        Map<Integer, TeamReferenceResponse> teamRanks = new TreeMap<>(Collections.reverseOrder());
-        for (Team team : teams) {
-            List<DayGardenResponse> gardenResponses = gardenQueryService.getThisWeekGarden(team.getId());
-            Integer point = gardenResponses.stream()
-                    .mapToInt(DayGardenResponse::contributeCount)
-                    .sum();
-            teamRanks.put(point, TeamReferenceResponse.from(team));
-        }
-        return teamRanks;
+    private int calculatePoint(final Team team) {
+        final List<DayGardenResponse> weekGardenResponses = gardenQueryService.getThisWeekGarden(team.getId());
+        return weekGardenResponses.stream()
+                .mapToInt(DayGardenResponse::contributeCount)
+                .sum();
     }
 }
--- src/main/java/doore/team/application/dto/response/TeamRankResponse.java
+++ src/main/java/doore/team/application/dto/response/TeamRankResponse.java
@@ -1,7 +1,11 @@
 package doore.team.application.dto.response;
 
+import doore.garden.application.dto.response.DayGardenResponse;
+import java.util.List;
+
 public record TeamRankResponse(
         int point,
-        TeamReferenceResponse teamReferenceResponse
+        TeamReferenceResponse teamReferenceResponse,
+        List<DayGardenResponse> teamGardenResponse
 ) {
 }
