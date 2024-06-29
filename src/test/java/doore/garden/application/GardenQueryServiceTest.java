7b123c6abaa26029bf7d3cf8d8eb72ee9f491f88
lcqff <yeonnu82g@gmail.com>
2024-06-29 22:51:59 +0900

    feat: 조회한 텃밭 데이터의 정렬 로직과 관련된 테스트를 삭제한다.
    #164

--- src/test/java/doore/garden/application/GardenQueryServiceTest.java
+++ src/test/java/doore/garden/application/GardenQueryServiceTest.java
@@ -63,50 +63,6 @@ public class GardenQueryServiceTest extends IntegrationTest {
     }
 
     @Test
-    @DisplayName("[성공] 팀의 텃밭은 기여된 날짜를 기준으로 오름차 정렬된다.")
-    public void getGardens_팀의_텃밭은_기여된_날짜를_기준으로_오름차_정렬된다_성공() throws Exception {
-        //given
-        final Long teamId = 1L;
-        final LocalDate now = LocalDate.now();
-        final LocalDate before1Week = now.minusWeeks(1);
-        final LocalDate before2Week = now.minusWeeks(2);
-        final Garden garden = Garden.builder()
-                .contributedDate(now)
-                .teamId(teamId)
-                .memberId(1L)
-                .contributionId(1L)
-                .type(GardenType.DOCUMENT_UPLOAD)
-                .build();
-        final Garden before1WeekGarden = Garden.builder()
-                .contributedDate(before1Week)
-                .teamId(teamId)
-                .memberId(1L)
-                .contributionId(2L)
-                .type(GardenType.STUDY_CURRICULUM_COMPLETION)
-                .build();
-        final Garden before2WeekGarden = Garden.builder()
-                .contributedDate(before2Week)
-                .teamId(teamId)
-                .memberId(1L)
-                .contributionId(3L)
-                .type(GardenType.STUDY_CURRICULUM_COMPLETION)
-                .build();
-
-        gardenRepository.saveAll(List.of(before1WeekGarden, before2WeekGarden, garden));
-
-        //when
-        final List<Garden> allGardens = gardenRepository.findAll();
-        final List<DayGardenResponse> gardenResponses = gardenQueryService.getGardens(teamId);
-
-        //then
-        assertAll(() -> {
-            assertEquals(now, gardenResponses.get(0).contributeDate());
-            assertEquals(before1Week, gardenResponses.get(1).contributeDate());
-            assertEquals(before2WeekGarden, gardenResponses.get(2).contributeDate());
-        });
-    }
-
-    @Test
     @DisplayName("[성공] 정상적으로 동일한 날의 기여도를 계산할 수 있다.")
     public void calculateContributes_정상적으로_동일한_날의_기여도를_계산할_수_있다_성공() throws Exception {
         //given
