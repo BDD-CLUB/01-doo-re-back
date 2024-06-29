c5e709de6ba3ad486e0fca7964fac897e52f8248
JJimini <min102602@pusan.ac.kr>
2024-06-30 02:01:50 +0900

    feat: 팀 상세조회에 teamLeaderId 정보 추가

--- src/main/java/doore/member/domain/repository/TeamRoleRepository.java
+++ src/main/java/doore/member/domain/repository/TeamRoleRepository.java
@@ -4,9 +4,12 @@ import doore.member.domain.TeamRole;
 import doore.member.domain.TeamRoleType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TeamRoleRepository extends JpaRepository<TeamRole, Long> {
     Optional<TeamRole> findTeamRoleByTeamIdAndMemberId(Long teamId, Long memberId);
     Optional<TeamRole> findTeamRoleByTeamIdAndTeamRoleType(Long teamId, TeamRoleType teamRoleType);
     Optional<TeamRole> findTeamRoleByMemberId(Long memberId);
+    @Query("SELECT tr.memberId FROM TeamRole tr WHERE tr.teamId = :teamId AND tr.teamRoleType = 'ROLE_팀장'")
+    Long findLeaderIdByTeamId(Long teamId);
 }
