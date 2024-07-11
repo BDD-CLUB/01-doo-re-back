75f2ab7fd08d8ec9c871cc8a97f07737c54a975b
lcqff <yeonnu82g@gmail.com>
2024-07-11 09:46:05 +0900

    feat: Crop 도메인 삭제
    #177

--- src/main/java/doore/crop/domain/repository/CropRepository.java
+++
@@ -1,8 +0,0 @@
-package doore.crop.domain.repository;
-
-import doore.crop.domain.Crop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CropRepository extends JpaRepository<Crop, Long> {
-
-}
