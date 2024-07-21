a0ca5556dd3a31c4da11318b52000c0e0e19ace9
JJimini <min102602@pusan.ac.kr>
2024-07-21 18:08:49 +0900

    refactor: findAll.size에서 count로 변경

--- src/main/java/doore/study/application/CurriculumItemCommandService.java
+++ src/main/java/doore/study/application/CurriculumItemCommandService.java
@@ -111,7 +111,7 @@ public class CurriculumItemCommandService {
     }
 
     private void createCurriculum(final Long studyId, final List<CurriculumItemManageDetailRequest> curriculumItems) {
-        if (curriculumItemRepository.findAll().size() >= 99) {
+        if (curriculumItemRepository.count() >= 99) {
             throw new CurriculumItemException(CANNOT_CREATE_CURRICULUM_ITEM);
         }
         curriculumItems.stream()
