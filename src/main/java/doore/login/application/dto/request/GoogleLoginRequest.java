4f56e776615ef91eea505d3a32c590115ec1ed6c
lcqff <yeonnu82g@gmail.com>
2024-08-14 11:05:52 +0900

    chore: snakeCase에서 camelCase로 변경한다.

--- src/main/java/doore/login/application/dto/request/GoogleLoginRequest.java
+++ src/main/java/doore/login/application/dto/request/GoogleLoginRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record GoogleLoginRequest(
         @NotNull(message = "리다이렉션 uri은 null일 수 없습니다.")
-        String redirect_uri,
+        String redirectUri,
         @NotNull(message = "인가 코드는 null일 수 없습니다.")
         String code
 ) {
