b9e6bd25ae15fdb7767b905ed38009155e807b14
JJimini <min102602@pusan.ac.kr>
2024-11-28 16:55:16 +0900

    fix: 팀원 삭제 시 teamRole도 삭제하는 로직 추가

--- src/main/java/doore/member/application/MemberTeamCommandService.java
+++ src/main/java/doore/member/application/MemberTeamCommandService.java
@@ -1,5 +1,6 @@
 package doore.member.application;
 
+import doore.member.application.convenience.TeamRoleConvenience;
 import doore.member.application.convenience.TeamRoleValidateAccessPermission;
 import doore.member.domain.repository.MemberTeamRepository;
 import doore.team.application.convenience.TeamValidateAccessPermission;
@@ -16,10 +17,13 @@ public class MemberTeamCommandService {
     private final TeamRoleValidateAccessPermission teamRoleValidateAccessPermission;
     private final TeamValidateAccessPermission teamValidateAccessPermission;
 
+    private final TeamRoleConvenience teamRoleConvenience;
+
     public void deleteMemberTeam(final Long teamId, final Long deleteMemberId, final Long teamLeaderId) {
         teamValidateAccessPermission.validateExistTeam(teamId);
         teamRoleValidateAccessPermission.validateExistTeamLeader(teamId, teamLeaderId);
         teamRoleValidateAccessPermission.validateExistMemberTeam(teamId, deleteMemberId);
         memberTeamRepository.deleteByTeamIdAndMemberId(teamId, deleteMemberId);
+        teamRoleConvenience.deleteByTeamIdAndMemberId(teamId, deleteMemberId);
     }
 }
