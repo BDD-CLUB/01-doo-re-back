7d873c28ab536b9adb279d5de3299badbaa55fb2
JJimini <min102602@pusan.ac.kr>
2024-11-30 00:15:29 +0900

    test: 추가로 개발된 사항에 대한 테스트 개발

--- src/test/java/doore/study/application/ParticipantCommandServiceTest.java
+++ src/test/java/doore/study/application/ParticipantCommandServiceTest.java
@@ -1,8 +1,11 @@
 package doore.study.application;
 
 import static doore.member.MemberFixture.createMember;
+import static doore.member.MemberFixture.미나;
 import static doore.member.MemberFixture.아마란스;
+import static doore.member.domain.TeamRoleType.ROLE_팀장;
 import static doore.member.exception.MemberExceptionType.UNAUTHORIZED;
+import static doore.member.exception.ParticipantExceptionType.CANNOT_DELETE_STUDY_LEADER_SELF;
 import static doore.study.StudyFixture.algorithmStudy;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,12 +14,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import doore.helper.IntegrationTest;
 import doore.member.domain.Member;
+import doore.member.domain.Participant;
 import doore.member.domain.StudyRole;
 import doore.member.domain.StudyRoleType;
+import doore.member.domain.TeamRole;
 import doore.member.domain.repository.MemberRepository;
 import doore.member.domain.repository.ParticipantRepository;
 import doore.member.domain.repository.StudyRoleRepository;
+import doore.member.domain.repository.TeamRoleRepository;
 import doore.member.exception.MemberException;
+import doore.member.exception.ParticipantException;
 import doore.study.application.dto.response.ParticipantResponse;
 import doore.study.domain.Study;
 import doore.study.domain.repository.StudyRepository;
@@ -40,19 +47,29 @@ public class ParticipantCommandServiceTest extends IntegrationTest {
     private StudyRoleRepository studyRoleRepository;
     @Autowired
     private ParticipantRepository participantRepository;
+    @Autowired
+    private TeamRoleRepository teamRoleRepository;
 
+    private Member teamLeader;
     private Member member;
     private Study study;
     private StudyRole studyRole;
+    private TeamRole teamRole;
 
     @BeforeEach
     void setUp() {
+        teamLeader = memberRepository.save(미나());
         member = memberRepository.save(아마란스());
         study = studyRepository.save(algorithmStudy());
         studyRole = studyRoleRepository.save(StudyRole.builder()
                 .studyRoleType(StudyRoleType.ROLE_스터디장)
                 .studyId(study.getId())
                 .memberId(member.getId())
+                .build());
+        teamRole = teamRoleRepository.save(TeamRole.builder()
+                .teamId(study.getTeamId())
+                .teamRoleType(ROLE_팀장)
+                .memberId(teamLeader.getId())
                 .build());
     }
 
@@ -85,7 +102,8 @@ public class ParticipantCommandServiceTest extends IntegrationTest {
             final Long notExistingMemberId = 50L;
 
             assertThatThrownBy(
-                    () -> participantCommandService.createParticipant(study.getId(), notExistingMemberId, member.getId()))
+                    () -> participantCommandService.createParticipant(study.getId(), notExistingMemberId,
+                            member.getId()))
                     .isInstanceOf(MemberException.class)
                     .hasMessage(UNAUTHORIZED.errorMessage());
         }
@@ -103,6 +121,36 @@ public class ParticipantCommandServiceTest extends IntegrationTest {
 
             //then
             assertThat(participantRepository.findByMemberId(participant.getId()).get(0).getIsDeleted()).isEqualTo(true);
+        }
+
+        @Test
+        @DisplayName("[실패] 스터디장은 스터디장 본인을 탈퇴 시킬 수 없다.")
+        public void deleteParticipant_스터디장은_스터디장_본인을_탈퇴_시킬_수_없다_실패() throws Exception {
+            assertThatThrownBy(() -> {
+                participantCommandService.deleteParticipant(study.getId(), member.getId(), member.getId());
+            }).isInstanceOf(ParticipantException.class).hasMessage(CANNOT_DELETE_STUDY_LEADER_SELF.errorMessage());
+        }
+
+        @Test
+        @DisplayName("[성공] 팀장은 스터디장을 탈퇴 시킬 수 있다.")
+        public void deleteParticipant_팀장은_스터디장을_탈퇴_시킬_수_있다_성공() {
+            participantRepository.save(Participant.builder().studyId(study.getId()).member(member).build());
+
+            participantCommandService.deleteParticipant(study.getId(), member.getId(), teamLeader.getId());
+
+            assertThat(participantRepository.findByMemberId(member.getId()).get(0).getIsDeleted()).isEqualTo(true);
+        }
+
+        @Test
+        @DisplayName("[성공] 팀장이 스터디장을 탈퇴 시킬 경우 스터디장 권한은 팀장에게 위임된다.")
+        public void deleteParticipant_팀장이_스터디장을_탈퇴_시킬_경우_스터디장_권한은_팀장에게_위임된다_성공() {
+            participantRepository.save(Participant.builder().studyId(study.getId()).member(member).build());
+            participantCommandService.deleteParticipant(study.getId(), member.getId(), teamLeader.getId());
+
+            StudyRole afterStudyLeader = studyRoleRepository.findStudyRoleByStudyIdAndMemberId(study.getId(),
+                    teamLeader.getId()).orElseThrow();
+
+            assertThat(afterStudyLeader.getMemberId()).isEqualTo(teamLeader.getId());
         }
 
         @Test
