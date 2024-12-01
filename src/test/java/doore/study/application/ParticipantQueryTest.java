d91c09dca22afe4fe27262e18ede05ec03d27360
JJimini <min102602@pusan.ac.kr>
2024-12-01 16:45:37 +0900

    fix: 실패 테스트 수정 - 테스트 시 createParticipant 호출로 인해 studyRole이 2번 저장되어 unique한 결과를 불러오지 못해 나타난 오류를 해결하였습니다.

--- src/test/java/doore/study/application/ParticipantQueryTest.java
+++ src/test/java/doore/study/application/ParticipantQueryTest.java
@@ -12,9 +12,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import doore.helper.IntegrationTest;
 import doore.member.domain.Member;
+import doore.member.domain.Participant;
 import doore.member.domain.StudyRole;
 import doore.member.domain.StudyRoleType;
 import doore.member.domain.repository.MemberRepository;
+import doore.member.domain.repository.ParticipantRepository;
 import doore.member.domain.repository.StudyRoleRepository;
 import doore.member.exception.MemberException;
 import doore.study.application.dto.response.ParticipantResponse;
@@ -31,6 +33,8 @@ public class ParticipantQueryTest extends IntegrationTest {
     private MemberRepository memberRepository;
     @Autowired
     private StudyRepository studyRepository;
+    @Autowired
+    private ParticipantRepository participantRepository;
     @Autowired
     private ParticipantCommandService participantCommandService;
     @Autowired
@@ -90,8 +94,9 @@ public class ParticipantQueryTest extends IntegrationTest {
     @Test
     @DisplayName("[성공] 참여자를 삭제하면 참여자 목록 조회에 삭제된 참여자가 조회되지 않는다.")
     void getParticipants_참여자를_삭제하면_참여자_목록_조회에_삭제된_참여자가_조회되지_않는다_성공() {
-        participantCommandService.createParticipant(study.getId(), member.getId(), member.getId());
-        participantCommandService.createParticipant(study.getId(), otherMember.getId(), member.getId());
+        participantRepository.save(Participant.builder().member(member).studyId(study.getId()).build());
+        participantRepository.save(Participant.builder().member(otherMember).studyId(study.getId()).build());
+
         final List<ParticipantResponse> beforeParticipantResponses = participantQueryService.getParticipants(
                 study.getId(), member.getId());
 
