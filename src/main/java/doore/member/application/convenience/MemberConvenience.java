0f82d23ec6755f5520648960ed8073620f7d7f7b
JJimini <min102602@pusan.ac.kr>
2024-12-19 23:08:25 +0900

    feat: 팀장이 스터디장을 삭제한 후 해당 스터디에 위임되기 전 그 스터디의 참여자에 저장되도록 한다. - 그렇게 하지 않을 경우 위임된 스터디장(팀장)이 해당 스터디를 탈퇴할 때 오류가 발생하게 된다.

--- src/main/java/doore/member/application/convenience/MemberConvenience.java
+++ src/main/java/doore/member/application/convenience/MemberConvenience.java
@@ -1,7 +1,10 @@
 package doore.member.application.convenience;
 
+import static doore.member.exception.MemberExceptionType.NOT_FOUND_MEMBER;
 import static doore.member.exception.MemberExceptionType.UNAUTHORIZED;
 
+import doore.member.domain.Member;
+import doore.member.domain.repository.MemberRepository;
 import doore.member.exception.MemberException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class MemberConvenience {
+    private final MemberRepository memberRepository;
 
     public void checkSameMemberIdAndTokenMemberId(final Long memberId, final Long tokenMemberId) {
         if (!memberId.equals(tokenMemberId)) {
             throw new MemberException(UNAUTHORIZED);
         }
+    }
+
+    public Member findByMember(final Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
     }
 }
