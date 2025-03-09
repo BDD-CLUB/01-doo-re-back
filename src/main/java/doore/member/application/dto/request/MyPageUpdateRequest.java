4234f9d7dffb7a79b91b5959b4032071baffbac4
JJimini <min102602@pusan.ac.kr>
2025-03-09 11:00:27 +0900

    fix: NotNull 검증을 NotBlack 검증으로 변경

--- src/main/java/doore/member/application/dto/request/MyPageUpdateRequest.java
+++ src/main/java/doore/member/application/dto/request/MyPageUpdateRequest.java
@@ -1,9 +1,9 @@
 package doore.member.application.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record MyPageUpdateRequest(
-        @NotNull(message = "이름을 입력해주세요.")
+        @NotBlank(message = "이름을 입력해주세요.")
         String name
 ) {
 }
