90e921e4c75b099244428fad13954f471d954dc2
JJimini <min102602@pusan.ac.kr>
2025-03-12 11:40:43 +0900

    chore: 누락된 final 추가

--- src/main/java/doore/team/application/TeamQueryService.java
+++ src/main/java/doore/team/application/TeamQueryService.java
@@ -85,7 +85,7 @@ public class TeamQueryService {
 
     public List<TeamRankResponse> getTeamRanks() {
         final List<Team> teams = teamRepository.findAll();
-        List<TeamRankResponse> teamRanks = teams.stream().map(this::convertTeamToTeamRankResponse).toList();
+        final List<TeamRankResponse> teamRanks = teams.stream().map(this::convertTeamToTeamRankResponse).toList();
         return teamRanks.stream()
                 .sorted(Comparator.comparingInt(TeamRankResponse::point).reversed())
                 .toList();
