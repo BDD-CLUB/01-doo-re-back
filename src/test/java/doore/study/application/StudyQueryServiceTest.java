2a531f38f8ed3887d904f38fc4c37e40469682bd
JJimini <min102602@pusan.ac.kr>
2025-04-10 22:43:34 +0900

    test: 스터디 정렬 테스트 추가

--- src/test/java/doore/study/application/StudyQueryServiceTest.java
+++ src/test/java/doore/study/application/StudyQueryServiceTest.java
@@ -1,16 +1,25 @@
 package doore.study.application;
 
+import static doore.document.domain.DocumentGroupType.STUDY;
+import static doore.document.domain.DocumentGroupType.TEAM;
+import static doore.document.domain.DocumentType.URL;
 import static doore.member.MemberFixture.미나;
 import static doore.member.domain.StudyRoleType.ROLE_스터디장;
 import static doore.member.exception.MemberExceptionType.UNAUTHORIZED;
 import static doore.study.CurriculumItemFixture.curriculumItem;
 import static doore.study.ParticipantCurriculumItemFixture.participantCurriculumItem;
 import static doore.study.StudyFixture.algorithmStudy;
+import static doore.study.StudyFixture.endedStudy;
+import static doore.study.StudyFixture.inProgressStudy;
+import static doore.study.StudyFixture.upComingStudy;
 import static doore.study.exception.StudyExceptionType.NOT_FOUND_STUDY;
 import static doore.team.TeamFixture.team;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import doore.document.DocumentFixture;
+import doore.document.domain.Document;
+import doore.document.domain.repository.DocumentRepository;
 import doore.helper.IntegrationTest;
 import doore.member.domain.Member;
 import doore.member.domain.Participant;
@@ -19,6 +28,7 @@ import doore.member.domain.repository.MemberRepository;
 import doore.member.domain.repository.ParticipantRepository;
 import doore.member.domain.repository.StudyRoleRepository;
 import doore.member.exception.MemberException;
+import doore.study.application.dto.response.StudyRankResponse;
 import doore.study.application.dto.response.StudyReferenceResponse;
 import doore.study.application.dto.response.StudyResponse;
 import doore.study.domain.CurriculumItem;
@@ -36,6 +46,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 public class StudyQueryServiceTest extends IntegrationTest {
     @Autowired
@@ -56,6 +69,8 @@ public class StudyQueryServiceTest extends IntegrationTest {
     private StudyRoleRepository studyRoleRepository;
     @Autowired
     private ParticipantCurriculumItemRepository participantCurriculumItemRepository;
+    @Autowired
+    private DocumentRepository documentRepository;
 
     private Member member;
     private StudyRole studyRole;
@@ -156,5 +171,242 @@ public class StudyQueryServiceTest extends IntegrationTest {
         assertThatThrownBy(() -> {
             studyQueryService.getMyStudies(member.getId(), anotherMemberId);
         }).isInstanceOf(MemberException.class).hasMessage(UNAUTHORIZED.errorMessage());
+    }
+
+    @Nested
+    @DisplayName("스터디 정렬 테스트")
+    class StudySortingTest {
+        private Study inProgressHighPointStudy;
+        private Study inProgressMediumPointStudy;
+        private Study inProgressLowPointStudy;
+        private Study upComingHighPointStudy;
+        private Study upComingMediumPointStudy;
+        private Study upComingLowPointStudy;
+        private Study endedHighPointStudy;
+        private Study endedMediumPointStudy;
+        private Study endedLowPointStudy;
+
+        private Document inProgressDocument;
+        private Document upComingDocument;
+        private Document endedDocument;
+
+        private CurriculumItem inProgressCurriculumItem;
+        private CurriculumItem upComingCurriculumItem;
+        private CurriculumItem endedCurriculumItem;
+
+        @BeforeEach
+        void setUp() {
+            studyRepository.deleteAll(); // "스터디 정렬 테스트"에서 생성된 스터디만 고려
+
+            endedLowPointStudy = studyRepository.save(endedStudy());
+            endedMediumPointStudy = studyRepository.save(endedStudy());
+            endedHighPointStudy = studyRepository.save(endedStudy());
+
+            inProgressLowPointStudy = studyRepository.save(inProgressStudy());
+            inProgressMediumPointStudy = studyRepository.save(inProgressStudy());
+            inProgressHighPointStudy = studyRepository.save(inProgressStudy());
+
+            upComingLowPointStudy = studyRepository.save(upComingStudy());
+            upComingMediumPointStudy = studyRepository.save(upComingStudy());
+            upComingHighPointStudy = studyRepository.save(upComingStudy());
+
+            inProgressDocument = new DocumentFixture()
+                    .groupType(STUDY)
+                    .groupId(inProgressMediumPointStudy.getId())
+                    .type(URL)
+                    .uploaderId(member.getId())
+                    .buildDocument();
+            upComingDocument = new DocumentFixture()
+                    .groupType(STUDY)
+                    .groupId(upComingMediumPointStudy.getId())
+                    .type(URL)
+                    .uploaderId(member.getId())
+                    .buildDocument();
+            endedDocument = new DocumentFixture()
+                    .groupType(STUDY)
+                    .groupId(endedMediumPointStudy.getId())
+                    .type(URL)
+                    .uploaderId(member.getId())
+                    .buildDocument();
+
+            participantRepository.save(
+                    Participant.builder().studyId(inProgressHighPointStudy.getId()).member(member).build());
+            participantRepository.save(
+                    Participant.builder().studyId(upComingHighPointStudy.getId()).member(member).build());
+            participantRepository.save(
+                    Participant.builder().studyId(endedHighPointStudy.getId()).member(member).build());
+            studyRoleRepository.save(
+                    StudyRole.builder().studyRoleType(ROLE_스터디장).studyId(inProgressHighPointStudy.getId())
+                            .memberId(member.getId()).build());
+            studyRoleRepository.save(
+                    StudyRole.builder().studyRoleType(ROLE_스터디장).studyId(upComingHighPointStudy.getId())
+                            .memberId(member.getId()).build());
+            studyRoleRepository.save(
+                    StudyRole.builder().studyRoleType(ROLE_스터디장).studyId(endedHighPointStudy.getId())
+                            .memberId(member.getId()).build());
+
+            inProgressCurriculumItem = curriculumItemRepository.save(CurriculumItem.builder()
+                    .study(inProgressHighPointStudy)
+                    .itemOrder(1)
+                    .name("IN-PROGRESS curriculum")
+                    .build());
+            upComingCurriculumItem = curriculumItemRepository.save(CurriculumItem.builder()
+                    .study(upComingHighPointStudy)
+                    .itemOrder(1)
+                    .name("UPCOMING curriculum")
+                    .build());
+            endedCurriculumItem = curriculumItemRepository.save(CurriculumItem.builder()
+                    .study(endedHighPointStudy)
+                    .itemOrder(1)
+                    .name("ENDED curriculum")
+                    .build());
+
+            participantCurriculumItemRepository.save(ParticipantCurriculumItem.builder()
+                    .participantId(member.getId())
+                    .curriculumItem(inProgressCurriculumItem)
+                    .build());
+            participantCurriculumItemRepository.save(ParticipantCurriculumItem.builder()
+                    .participantId(member.getId())
+                    .curriculumItem(upComingCurriculumItem)
+                    .build());
+            participantCurriculumItemRepository.save(ParticipantCurriculumItem.builder()
+                    .participantId(member.getId())
+                    .curriculumItem(endedCurriculumItem)
+                    .build());
+        }
+
+        @Test
+        @DisplayName("[성공] 스터디가 IN-PROGRESS, UPCOMING, ENDED 순으로 정렬된다.")
+        void getTeamStudies_스터디가_IN_PROGRESS_UPCOMING_ENDED_순으로_정렬된다_성공() {
+            final Pageable pageable = PageRequest.of(0, 10);
+            final Page<StudyRankResponse> responses = studyQueryService.getTeamStudies(team.getId(), pageable);
+            final List<StudyRankResponse> studyList = responses.getContent();
+
+            assertThat(studyList.get(0).studyReferenceResponse().status()).isEqualTo(
+                    inProgressMediumPointStudy.getStatus());
+            assertThat(studyList.get(1).studyReferenceResponse().status()).isEqualTo(
+                    inProgressLowPointStudy.getStatus());
+            assertThat(studyList.get(2).studyReferenceResponse().status()).isEqualTo(
+                    inProgressHighPointStudy.getStatus());
+
+            assertThat(studyList.get(3).studyReferenceResponse().status()).isEqualTo(
+                    upComingMediumPointStudy.getStatus());
+            assertThat(studyList.get(4).studyReferenceResponse().status()).isEqualTo(
+                    upComingLowPointStudy.getStatus());
+            assertThat(studyList.get(5).studyReferenceResponse().status()).isEqualTo(
+                    upComingHighPointStudy.getStatus());
+
+            assertThat(studyList.get(6).studyReferenceResponse().status()).isEqualTo(
+                    endedMediumPointStudy.getStatus());
+            assertThat(studyList.get(7).studyReferenceResponse().status()).isEqualTo(
+                    endedLowPointStudy.getStatus());
+            assertThat(studyList.get(8).studyReferenceResponse().status()).isEqualTo(
+                    endedHighPointStudy.getStatus());
+        }
+
+        @Test
+        @DisplayName("[성공] 스터디가 상태 정렬 내에서는 활발도 기준으로 정렬된다.")
+            // 커리큘럼 완성도 + 스터디 학습자료 개수
+        void getTeamStudies_스터디가_상태_정렬_내에서는_활발도_기준으로_정렬된다_성공() {
+            curriculumItemCommandService.checkCurriculum(inProgressCurriculumItem.getId(), member.getId(),
+                    member.getId());
+            curriculumItemCommandService.checkCurriculum(upComingCurriculumItem.getId(), member.getId(),
+                    member.getId());
+            curriculumItemCommandService.checkCurriculum(endedCurriculumItem.getId(), member.getId(),
+                    member.getId());
+
+            final Pageable pageable = PageRequest.of(0, 10);
+            final Page<StudyRankResponse> responses = studyQueryService.getTeamStudies(team.getId(), pageable);
+            final List<StudyRankResponse> studyList = responses.getContent();
+
+            assertThat(studyList.get(0).studyReferenceResponse().id()).isEqualTo(
+                    inProgressHighPointStudy.getId()); // 100
+            assertThat(studyList.get(1).studyReferenceResponse().id()).isEqualTo(
+                    inProgressMediumPointStudy.getId()); // 1
+            assertThat(studyList.get(2).studyReferenceResponse().id()).isEqualTo(
+                    inProgressLowPointStudy.getId()); // 0
+
+            assertThat(studyList.get(3).studyReferenceResponse().id()).isEqualTo(
+                    upComingHighPointStudy.getId()); // 100
+            assertThat(studyList.get(4).studyReferenceResponse().id()).isEqualTo(
+                    upComingMediumPointStudy.getId()); // 1
+            assertThat(studyList.get(5).studyReferenceResponse().id()).isEqualTo(
+                    upComingLowPointStudy.getId()); // 0
+
+            assertThat(studyList.get(6).studyReferenceResponse().id()).isEqualTo(
+                    endedHighPointStudy.getId()); // 100
+            assertThat(studyList.get(7).studyReferenceResponse().id()).isEqualTo(
+                    endedMediumPointStudy.getId()); // 1
+            assertThat(studyList.get(8).studyReferenceResponse().id()).isEqualTo(
+                    endedLowPointStudy.getId()); // 0
+        }
+
+        @Test
+        @DisplayName("[성공] 커리큘럼 체크 개수를 기반으로 커리큘럼 진행도를 계산할 수 있다.")
+        void checkStudyProgressRatio_커리큘럼_체크_개수를_기반으로_커리큘럼_진행도를_계산할_수_있다_성공() {
+            final ParticipantCurriculumItem previousCurriculum = participantCurriculumItemRepository.findById(
+                    inProgressCurriculumItem.getId()).orElseThrow();
+
+            assertThat(previousCurriculum.getIsChecked()).isEqualTo(false);
+
+            curriculumItemCommandService.checkCurriculum(inProgressCurriculumItem.getId(), member.getId(),
+                    member.getId());
+            final Pageable pageable = PageRequest.of(0, 10);
+            final Page<StudyRankResponse> responses = studyQueryService.getTeamStudies(team.getId(), pageable);
+            final List<StudyRankResponse> studyList = responses.getContent();
+            final ParticipantCurriculumItem afterCurriculum = participantCurriculumItemRepository.findById(
+                    inProgressCurriculumItem.getId()).orElseThrow();
+
+            assertThat(afterCurriculum.getIsChecked()).isEqualTo(true);
+            assertThat(studyList.get(0).studyReferenceResponse().id()).isEqualTo(inProgressHighPointStudy.getId());
+            assertThat(studyList.get(0).point()).isEqualTo(100);
+        }
+
+        @Test
+        @DisplayName("[성공] 커리큘럼 진행도와 스터디 학습자료 개수를 기반으로 포인트를 계산할 수 있다.")
+        void calculatePoint_커리큘럼_진행도와_스터디_학습자료_개수를_기반으로_포인트를_계산할_수_있다_성공() {
+            final Document otherInProgressDocument = new DocumentFixture()
+                    .groupType(STUDY)
+                    .groupId(inProgressHighPointStudy.getId())
+                    .type(URL)
+                    .uploaderId(member.getId())
+                    .buildDocument();
+
+            curriculumItemCommandService.checkCurriculum(inProgressCurriculumItem.getId(), member.getId(),
+                    member.getId());
+            final Pageable pageable = PageRequest.of(0, 4);
+            final Page<StudyRankResponse> responses = studyQueryService.getTeamStudies(team.getId(), pageable);
+            final List<StudyRankResponse> studyList = responses.getContent();
+            final ParticipantCurriculumItem afterCurriculum = participantCurriculumItemRepository.findById(
+                    inProgressCurriculumItem.getId()).orElseThrow();
+
+            assertThat(afterCurriculum.getIsChecked()).isEqualTo(true);
+            assertThat(studyList.get(0).point()).isEqualTo(101);
+        }
+
+        @Test
+        @DisplayName("[성공] 팀 학습자료는 포인트에 합산되지 않는다.")
+        void calculatePoint_팀_학습자료는_포인트에_합산되지_않는다_성공() {
+            final Pageable pageable = PageRequest.of(0, 4);
+            final Page<StudyRankResponse> beforeResponses = studyQueryService.getTeamStudies(team.getId(), pageable);
+            final List<StudyRankResponse> beforeStudyList = beforeResponses.getContent();
+            assertThat(beforeStudyList.get(0).studyReferenceResponse().id()).isEqualTo(
+                    inProgressMediumPointStudy.getId());
+            assertThat(beforeStudyList.get(0).point()).isEqualTo(1);
+
+            final Document otherInProgressDocument = new DocumentFixture()
+                    .groupType(TEAM)
+                    .groupId(inProgressMediumPointStudy.getTeamId())
+                    .type(URL)
+                    .uploaderId(member.getId())
+                    .buildDocument();
+
+            final Page<StudyRankResponse> afterResponses = studyQueryService.getTeamStudies(team.getId(), pageable);
+            final List<StudyRankResponse> afterStudyList = afterResponses.getContent();
+
+            assertThat(afterStudyList.get(0).studyReferenceResponse().id()).isEqualTo(
+                    inProgressMediumPointStudy.getId());
+            assertThat(afterStudyList.get(0).point()).isEqualTo(1);
+        }
     }
 }
