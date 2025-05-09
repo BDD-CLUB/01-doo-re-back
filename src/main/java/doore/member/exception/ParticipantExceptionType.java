cce3a16e8212e6099661e2cf8df538aa9a4fc3de
JIMIN LEE <107912763+JJimini@users.noreply.github.com>
2025-05-09 10:57:33 +0900

    Feature/#240 스터디원 중복 추가 불가 기능 추가 (#297)
    * feat : 참여자 중복 추가가 불가하도록 변경
    
    * test: 참여자 중복 추가 불가 기능에 대한 테스트 추가

--- src/main/java/doore/member/exception/ParticipantExceptionType.java
+++ src/main/java/doore/member/exception/ParticipantExceptionType.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 public enum ParticipantExceptionType implements BaseExceptionType {
 
     CANNOT_DELETE_STUDY_LEADER_SELF(HttpStatus.BAD_REQUEST, "스터디장 본인은 팀을 탈퇴할 수 없습니다."),
+    ALREADY_JOINED_STUDY(HttpStatus.BAD_REQUEST, "이미 참여중인 스터디입니다."),
     ;
 
     private final HttpStatus httpStatus;
