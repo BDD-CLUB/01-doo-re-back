e60dc2ce311a495d8448a54e8ccbbeb09fe79ef4
JJimini <min102602@pusan.ac.kr>
2025-05-24 23:34:13 +0900

    fix: 탈퇴한 스터디는 조회되지 않도록 변경 후 테스트 수정

--- src/test/java/doore/study/application/ParticipantCommandServiceTest.java
+++ src/test/java/doore/study/application/ParticipantCommandServiceTest.java
@@ -175,7 +175,7 @@ public class ParticipantCommandServiceTest extends IntegrationTest {
             participantCommandService.deleteParticipant(studyId, participant.getId(), member.getId());
 
             //then
-            assertThat(participantRepository.findByMemberId(participant.getId()).get(0).getIsDeleted()).isEqualTo(true);
+            assertThat(participantRepository.findByMemberIdAndIsDeletedFalse(participant.getId()).size()).isEqualTo(0);
         }
 
         @Test
@@ -193,7 +193,7 @@ public class ParticipantCommandServiceTest extends IntegrationTest {
 
             participantCommandService.deleteParticipant(study.getId(), member.getId(), teamLeader.getId());
 
-            assertThat(participantRepository.findByMemberId(member.getId()).get(0).getIsDeleted()).isEqualTo(true);
+            assertThat(participantRepository.findByMemberIdAndIsDeletedFalse(member.getId()).size()).isEqualTo(0);
         }
 
         @Test
@@ -225,7 +225,7 @@ public class ParticipantCommandServiceTest extends IntegrationTest {
             participantCommandService.withdrawParticipant(studyId, participant.getId());
 
             //then
-            assertThat(participantRepository.findByMemberId(participant.getId()).get(0).getIsDeleted()).isEqualTo(true);
+            assertThat(participantRepository.findByMemberIdAndIsDeletedFalse(participant.getId()).size()).isEqualTo(0);
         }
     }
 
