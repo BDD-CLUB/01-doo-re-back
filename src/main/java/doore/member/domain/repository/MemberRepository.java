252ec86ebb265b643cfe9274db6c5ca77f8fb061
JIMIN LEE <107912763+JJimini@users.noreply.github.com>
2025-07-01 21:00:31 +0900

    Feature/#310 회원 탈퇴 시 학습자료 조회에 문제가 생기는 상황 해결 (#311)
    * fix: 탈퇴한 회원의 이름도 조회할 수 있도록 수정
    - native query를 사용해서 제한적으로 soft delete된 데이터를 조회
    
    * test: 탈퇴회원 학습자료 조회에 대한 테스트 코드 추가

--- src/main/java/doore/member/domain/repository/MemberRepository.java
+++ src/main/java/doore/member/domain/repository/MemberRepository.java
@@ -3,8 +3,13 @@ package doore.member.domain.repository;
 import doore.member.domain.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByGoogleId(final String googleId);
+
+    @Query(value = "SELECT name FROM member WHERE id = :id", nativeQuery = true)
+    Optional<String> findNameByIdNative(@Param("id") final Long memberId);
 }
